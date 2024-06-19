bdd1b0cbec86dda695014c154c31446389eb9c70
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-06-19 16:16:09 +0200

    added pros and tasks

---
+++ server/routes/tasksRouter.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import * as taskController from "../controllers/tasks.js";
+
+const tasksRouter = Router();
+
+tasksRouter
+  .route("/")
+  .get(taskController.getAllTasks)
+  .post(taskController.addNewTask);
+
+tasksRouter
+  .route("/:id")
+  .get(taskController.getTaskById)
+  .put(taskController.updateTask)
+  .delete(taskController.deleteTask);
+
+// tasksRouter.patch("/:id", taskController.addTagToTask);
+
+export default tasksRouter;
