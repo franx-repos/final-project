6a68e41a4a38eaf8c4d0ef25e63665d67bebce1a
Dsnirl Jim <Jens.Daniel.Klein@gmail.com>
2024-06-24 13:55:13 +0200

    first verification push

--- server/routes/clientsRouter.js
+++ server/routes/clientsRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import * as clientController from "../controllers/clients.js";
+import * as clientController from "../controllers/clients.js"
+import * as authClient from "../controllers/authclient.js";
 
 const clientsRouter = Router();
 
@@ -13,7 +14,10 @@ clientsRouter
   .get(clientController.getClientById)
   .put(clientController.updateClient)
   .delete(clientController.deleteClient);
+  // clientsRouter.patch("/:id", clientController.addTagToClient);
 
-// clientsRouter.patch("/:id", clientController.addTagToClient);
+  clientsRouter.post("/register", authClient.signUp);
+  clientsRouter.post("/Login",authClient.logIn);
+
 
 export default clientsRouter;
