65d49ed8046c71b56d107eca2cb8bc05c75852d4
aliwdg16 <alialtoto@hotmail.com>
2024-06-24 09:42:41 +0200

    tailwind pkg

--- client/src/components/TaxMax.jsx
+++ client/src/components/TaxMax.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
 function TaxMax() {
-  return <div>TaxMax</div>;
+  return 
+  <div>   
+   <h1 className="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+</div>;
 }
+
 
 export default TaxMax;
