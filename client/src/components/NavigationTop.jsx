a25b6032b4b26f0057b857997a5734c716bc6b6b
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-06-27 10:52:55 +0200

    test

--- client/src/components/NavigationTop.jsx
+++ client/src/components/NavigationTop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/UserProvider";
 import axios from "axios";
 // import { useAuth } from "../context/UserProvider.jsx";
