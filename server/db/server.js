46abbe1327d856886e209e3c33765c0eb11ce853
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-07-02 11:12:45 +0200

    dfh

--- server/db/server.js
+++ server/db/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   console.log(process.env.MONGO_URI);
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { family: 4 });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Connection error:", error.stack);
