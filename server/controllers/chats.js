fc25fb92f2e0236aaacf73a02a8e80da6007b3e7
Tarek <tael3424@th-wildau.de>
2024-07-02 20:34:34 +0200

    update chat and sort the chat array in pro and client

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -1,17 +1,23 @@
 import Chat from "../models/chatSchema.js";
+import Client from "../models/clientsSchema.js";
+import Pro from "../models/prosSchema.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 export const updateChat = async (req, res, next) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const { cid, body } = req;
-  const { messages } = body;
-  console.log(cid)
-  console.log(messages)
+  const { message } = body;
+
   try {
+    const newMessage = {
+      author_id: cid,
+      text: message,
+    };
+
     const updateChat = await Chat.findByIdAndUpdate(
       id,
-      {messages:messages },
+      { $push: { messages: newMessage } },
       { new: true }
     );
 
@@ -19,21 +25,36 @@ export const updateChat = async (req, res, next) => {
       throw { statusCode: 404 };
     }
 
+    // Update the client's chat array
+    const clientUpdate = await Client.findByIdAndUpdate(updateChat.client, {
+      $pull: { chats: id },
+    });
+    await Client.findByIdAndUpdate(updateChat.client, {
+      $push: { chats: id },
+    });
+
+    // Update the pro's chat array
+    const proUpdate = await Pro.findByIdAndUpdate(updateChat.pro, {
+      $pull: { chats: id },
+    });
+    await Pro.findByIdAndUpdate(updateChat.pro, {
+      $push: { chats: id },
+    });
+
     res.json(updateChat);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
-
 export const getChatByClientID = async (req, res, next) => {
   const { cid } = req;
-  
+
   try {
     const chat = await Chat.find({ client: cid });
     res.status(200).json(chat);
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
 };
 
@@ -72,35 +93,63 @@ export const getAllChats = async (req, res, next) => {
 
 //Create a Chatroom this happens automatically as a professional and client connect
 export const createNewChat = asyncHandler(async (req, res, next) => {
-  const {cid, body} = req;
-  const {client, task} = body;
+  const { cid, body } = req;
+  const { client, task } = body;
   // const { client_id, pro_id, task_id } = req.body;
 
-  const newChat = await Chat.create({task:task, client: client, pro: cid})
+  const newChat = await Chat.create({ task: task, client: client, pro: cid });
+
+  //Update the client's chat array
+  const clientUpdate = await Client.findByIdAndUpdate(client, {
+    $push: { chats: newChat._id },
+  });
+
+  //Update the pro's chat array
+  const proUpdate = await Pro.findByIdAndUpdate(cid, {
+    $push: { chats: newChat._id },
+  });
 
   const populatedChat = await Chat.findById(newChat._id)
-  .populate('client')
-  .populate('task')
-  .populate('pro');
+    .populate("client")
+    .populate("task")
+    .populate("pro");
 
   res.status(201).json(populatedChat);
-
 });
 
-export const deleteChat = asyncHandler(async (req, res, next)=>{
+export const deleteChat = asyncHandler(async (req, res, next) => {
   const {
-    params : {id},
-    cid
+    params: { id },
+    cid,
   } = req;
 
-  const found = Chat.findById(id);
+  try {
+    const found = await Chat.findById(id);
+    console.log(found);
+    console.log(cid);
 
-  if (!found) throw new ErrorResponse(`Post ${id} does not exist`, 404);
+    if (!found) throw new ErrorResponse(`Post ${id} does not exist`, 404);
 
-  if (cid !== found.client_id.toString())
-    throw new ErrorResponse('You have no permission to delete this post', 401);
+    if (cid !== found.pro.toString() && cid !== found.client.toString())
+      throw new ErrorResponse(
+        "You have no permission to delete this post",
+        401
+      );
 
-  await Chat.findByIdAndDelete(id, body, {new:true}).populate('client');
-  res.json({success: `Post ${id} was deleted`});
+    await Chat.findByIdAndDelete(id);
 
-})
+    // Update the client's chat array
+    await Client.findByIdAndUpdate(found.client, {
+      $pull: { chats: id },
+    });
+
+    // Update the pro's chat array
+    await Pro.findByIdAndUpdate(found.pro, {
+      $pull: { chats: id },
+    });
+
+    res.json({ success: `Chat ${id} was deleted` });
+  } catch (error) {
+    next(error);
+  }
+});
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -4,20 +4,20 @@ import verifyToken from "../middlewares/verifyToken.js";
 
 const chatRouter = Router();
 
-
-chatRouter.route("/client_chat/").get(verifyToken,chatController.getChatByClientID);
+chatRouter
+  .route("/client_chat/")
+  .get(verifyToken, chatController.getChatByClientID);
 chatRouter.route("/pro_chat/").get(verifyToken, chatController.getChatByProID);
 
 chatRouter
   .route("/")
   .get(chatController.getAllChats)
-  .post(verifyToken,chatController.createNewChat);
+  .post(verifyToken, chatController.createNewChat);
 
 chatRouter
   .route("/:id")
-  .get(verifyToken,chatController.getChatById)
-  .patch(verifyToken,chatController.updateChat);
-
-
+  .get(verifyToken, chatController.getChatById)
+  .patch(verifyToken, chatController.updateChat)
+  .delete(verifyToken, chatController.deleteChat);
 
 export default chatRouter;
