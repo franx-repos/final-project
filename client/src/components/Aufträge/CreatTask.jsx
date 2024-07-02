833062b98d14f08c9f29f21d365f0079232e749a
Dsnirl Jim <Jens.Daniel.Klein@gmail.com>
2024-07-02 20:35:17 +0200

    creatTask changed

--- client/src/components/Aufträge/CreatTask.jsx
+++ client/src/components/Aufträge/CreatTask.jsx
@@ -1,21 +1,32 @@
 import axios from 'axios';
 import { useState, useRef, useEffect } from 'react';
-
+import Select from 'react-select';
 
 const NewPost = () => {
   const [images, setImages] = useState([]);
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [deadline, setDeadline] = useState("");
-    const [task_type, setTask_type] = useState("");
-    const [industry, setIndustry] = useState("");
-    const [create_date, setCreate_date] = useState("");
-    const [payment, setPayment] = useState(false);
-    const [documents, setDocuments] = useState("");
-    const [error, setError] = useState("");
-    const [file, setFile] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [task_type, setTask_type] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [create_date, setCreate_date] = useState("");
+  const [payment, setPayment] = useState(false);
+  const [documents, setDocuments] = useState("");
+  const [error, setError] = useState("");
+  const [file, setFile] = useState([]);
 
+  const options = [
+    { value: 'craft', label: 'craft' },
+    { value: 'it', label: 'IT' },
+    { value: 'gastronomy', label: 'gastronomy' },
+    { value: 'privat', label: 'privat' },
+    { value: 'other', label: 'other' },
+  ];
 
+  const types = [
+    { value: 'tax_declaration', label: 'tax declaration' },
+    { value: 'insolvency_law', label: 'insolvency law' },
+  ];
 
   const fileInputRef = useRef(null);
 
@@ -23,7 +34,7 @@ const NewPost = () => {
     const files = Array.from(event.target.files).map((file) => ({
       url: URL.createObjectURL(file),
       name: file.name,
-      preview: ['jpg', 'jpeg', 'png', 'gif','pdf' ].includes(file.name.split('.').pop().toLowerCase()),
+      preview: ['jpg', 'jpeg', 'png', 'gif', 'pdf'].includes(file.name.split('.').pop().toLowerCase()),
       size:
         file.size > 1024
           ? file.size > 1048576
@@ -38,74 +49,44 @@ const NewPost = () => {
     setImages(images.filter((_, i) => i !== index));
   };
 
+  const handlePost = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:8001/tasks`,
+        {
+          content: {
+            title,
+            description,
+            deadline,
+            task_type,
+            industry,
+            payment,
+          },
+          documents: [],
+        },
+        { withCredentials: true }
+      );
 
+      if (response.status === 200) {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      setError(error.message || "Something went wrong with Login");
+      console.log("Error:", error.message);
+      console.log("Error:", error.response.data);
+      if (error.response) {
+        console.log("Response data:", error.response.data);
+        console.log("Response status:", error.response.status);
+        console.log("Response headers:", error.response.headers);
+      }
+    }
+  };
 
-
-    // {
-    //     "content": {
-    //         "title": "Steuererklärung für Webdev",
-    //         "deadline": "2024-10-07T22:00:00.000Z",
-    //         "task_type": [
-    //             "Steuererklärung"
-    //         ],
-    //         "industry": [
-    //             "IT"
-    //         ],
-    //         "description": "Brauche naja Steuererklärung",
-    //         "create_date": "2024-06-26T07:20:37.128Z",
-    //         "payment": false,
-    //         "status": "OPEN"
-    //     },
-    //     "_id": "667bc145a84c56faf70cb0ce",
-    //     "documents": [],
-    //     "__v": 0
-    // }
-    const handlePost = async (e) => {
-        e.preventDefault();
-          try {
-            const response = await axios.post(
-              `http://localhost:8001/tasks`,
-              { 
-                content: {  title,
-                    description,
-                    deadline,
-                    task_type,
-                    industry,
-                    
-                    payment},
-                    documents :[],
-                    
-                    // documents:file.map ((file) => file.url),           
-            
-            },
-            { withCredentials: true }
-          );
-    
-          if (response.status === 200) {
-            setIsLoggedIn(true);
-            
-          }
-        } 
-        
-        catch (error) {
-          setError(error.message || "Something went wrong with Login");
-          console.log("Error:", error.message);
-          console.log("Error:", error.response.data);
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("Response data:", error.response.data);
-            console.log("Response status:", error.response.status);
-            console.log("Response headers:", error.response.headers);
-          }
-        }
-      };
-
-useEffect(() => {
-console.log('title:', title, 'description:', description, 'deadline:', deadline, 'task_type:', task_type, 'industry:', industry, 'create_date:', create_date, 'payment:', payment, 'documents:', documents)
-console.log(error)
-},[title, description, deadline, task_type, industry, create_date, payment, documents ,error]);
-
+  useEffect(() => {
+    console.log('title:', title, 'description:', description, 'deadline:', deadline, 'task_type:', task_type, 'industry:', industry, 'create_date:', create_date, 'payment:', payment, 'documents:', documents);
+    console.log(error);
+  }, [title, description, deadline, task_type, industry, create_date, payment, documents, error]);
 
   return (
     <form className="bg-white shadow p-4 py-8 dark:bg-[#1f2937]" onSubmit={handlePost}>
@@ -117,49 +98,50 @@ console.log(error)
           placeholder="Title"
           type="text"
           value={title}
-          onChange={(e)=>setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <textarea
           className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
           spellCheck="false"
           placeholder="Describe everything about this post here"
           value={description}
-          onChange={(e)=>setDescription(e.target.value)}
+          onChange={(e) => setDescription(e.target.value)}
         ></textarea>
 
+        <div className="sm:col-span-2 sm:col-start-1">
+          <label
+            htmlFor="industry"
+            className="block text-sm font-medium leading-6 text-gray-900 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Industry
+          </label>
+          <div className="mt-2">
+            <Select
+              options={options}
+              value={options.find(option => option.value === industry)}
+              onChange={(selectedOption) => setIndustry(selectedOption.value)}
+              placeholder="Choose one of the following"
+            />
+          </div>
+        </div>
 
-<div className="sm:col-span-2 sm:col-start-1">
-              <label
-                htmlFor="industry"
-                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              >
-                Industry
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="industry"
-                  id="industry"
-                  autoComplete="address-level2"
-                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm shadow-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={industry}
-                  required
-                  onChange={(e)=>setIndustry(e.target.value)}
-                />
-              </div>
-            </div>
+        <div className="sm:col-span-2 sm:col-start-1">
+          <label
+            htmlFor="task_type"
+            className="block text-sm font-medium leading-6 text-gray-900 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Type
+          </label>
+          <div className="mt-2">
+            <Select
+              options={types}
+              value={types.find(type => type.value === task_type)}
+              onChange={(selectedOption) => setTask_type(selectedOption.value)}
+              placeholder="Choose one of the following"
+            />
+          </div>
+        </div>
 
-<div  className="flex items-center mb-4">
-    <input id="default-radio-1" onChange={()=>setPayment(true)} type="radio" value={payment} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label htmlFor="default-radio-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Paid</label>
-</div>
-<div className="flex items-center">
-    <input checked id="default-radio-2" onChange={()=>setPayment(false)} type="radio" value={payment} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label htmlFor="default-radio-2" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Not Paid</label>
-</div>
-
-
-        {/* Icons */}
         <div className="icons flex text-gray-500 m-2">
           <label id="select-image">
             <svg
@@ -182,15 +164,10 @@ console.log(error)
               multiple
               onChange={handleFileChange}
               ref={fileInputRef}
-              value={file}
-              
-
             />
           </label>
-          {/* <div className="count ml-auto text-gray-400 text-xs font-semibold">0/300</div> */}
         </div>
 
-        {/* Preview image here */}
         <div id="preview" className="my-4 flex">
           {images.map((image, index) => (
             <div key={index} className="relative w-32 h-32 object-cover rounded">
@@ -228,9 +205,9 @@ console.log(error)
             </div>
           ))}
         </div>
-        {/* Buttons */}
-        <div className="buttons flex justify-end" >
-          <button type='submit' className="text-white bg-teal-500 hover:bg-teal-700  focus:outline-none font-medium rounded-lg text-sm mx-2 px-4 py-2 text-center dark:bg-teal-500 dark:hover:bg-teal-700">
+
+        <div className="buttons flex justify-end">
+          <button type='submit' className="text-white bg-teal-500 hover:bg-teal-700 focus:outline-none font-medium rounded-lg text-sm mx-2 px-4 py-2 text-center dark:bg-teal-500 dark:hover:bg-teal-700">
             Post
           </button>
         </div>
