3a53bbb2033c03beee73853cd85f3100e9a6f780
aliwdg16 <alialtoto@hotmail.com>
2024-07-08 17:24:07 +0200

    OurPartners

--- client/src/components/landing-page/OurPartners.jsx
+++ client/src/components/landing-page/OurPartners.jsx
@@ -110,8 +110,7 @@ function OurPartners() {
 
                     <p className="mb-2 h-[10rem] text-gray-500 dark:text-gray-400">
                       {pro.description}
-                      Here are the biggest enterprise technology acquisitions of
-                      2021 so far, in reverse chronological order.
+                     
                     </p>
 
                     {/* <p className="font-normal text-gray-700 dark:text-gray-400">
