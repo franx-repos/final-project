8076a8287ad55f070baad7f0adc02b2fddfdc1a3
aliwdg16 <alialtoto@hotmail.com>
2024-07-09 11:25:57 +0200

    befor reload to work

--- client/src/components/landing-page/HeroSection.jsx
+++ client/src/components/landing-page/HeroSection.jsx
@@ -2,7 +2,7 @@ import LineArt from "../../assets/LineArt";
 
 function HeroSection() {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-slate-200/90 dark:bg-gray-900 ">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold text-center tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -13,7 +13,7 @@ function HeroSection() {
           </p>
           <a
             href="#"
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-gray-900  hover:bg-gray-100 rounded focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Get started
             <svg
